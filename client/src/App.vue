ea876ef1315b619a76e9833db6324f4b05edfad8
sihamkassim <sihamkassim735@gmail.com>
2026-06-26 18:59:37 +0300

    Initial setup for vue

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div class="min-h-screen bg-gray-100 flex items-center justify-center">
+
+  <div 
+    class="bg-white p-8 rounded-2xl shadow-xl w-96"
+  >
+
+    <h1 class="text-3xl font-bold text-gray-800">
+      Vue + Tailwind 🔥
+    </h1>
+
+    <p class="mt-3 text-gray-600">
+      Tailwind 3.4.1 is working
+    </p>
+
+    <button 
+      class="mt-5 bg-blue-500 text-white px-5 py-2 rounded-lg hover:bg-blue-600"
+    >
+      Click me
+    </button>
+
+  </div>
+
+</div>
+
+</template>
